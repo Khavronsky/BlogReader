1db936e80139021a6e6901afbf62a6a6c9d2c141
khavronsky <khavronsky@gmail.com>
2016-12-26 16:14:48 +0300

    add selected method and other changes

--- app/src/main/java/com/khavronsky/blogreader/MainActivity.java
+++ app/src/main/java/com/khavronsky/blogreader/MainActivity.java
@@ -1,5 +1,7 @@
 package com.khavronsky.blogreader;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -10,10 +12,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     Toolbar toolbar;
     ActionBarDrawerToggle toggle;
+    FragmentTransaction fragmentTransaction;
+    Fragment firstFragment;
+    Fragment secondFragment;
+    Fragment thirdFragment;
+    Fragment fourthFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        firstFragment = new FirstFragment();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         toggle = new ActionBarDrawerToggle(
@@ -39,18 +48,69 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         if (id == R.id.fragment1) {
+            startFirstFragment();
 
         } else if (id == R.id.fragment2) {
+            startSecondFragment();
 
         } else if (id == R.id.fragment3) {
+            startThirdFragment();
 
         } else if (id == R.id.fragment4) {
-
+            startFourthFragment();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+
+    }
+
+    public void onFragmentButtonSelected(View v) {
+        int id = v.getId();
+        switch (id) {
+            case (R.id.first_button):
+                startSecondFragment();
+                break;
+            case (R.id.second_button):
+                startThirdFragment();
+                break;
+            case (R.id.third_button):
+                startFourthFragment();
+                break;
+            case (R.id.fourth_button):
+                startFirstFragment();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void startFirstFragment() {
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment1, firstFragment);
+        fragmentTransaction.commit();
+
+    }
+
+    public void startSecondFragment() {
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment1, secondFragment);
+        fragmentTransaction.commit();
+
+    }
+
+    public void startThirdFragment() {
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment1, thirdFragment);
+        fragmentTransaction.commit();
+
+    }
+
+    public void startFourthFragment() {
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment1, fourthFragment);
+        fragmentTransaction.commit();
 
     }
 
