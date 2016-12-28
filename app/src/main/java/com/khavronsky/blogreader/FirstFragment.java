0da75832a36fb9bfad09838b873488ca78cba8ac
khavronsky <khavronsky@gmail.com>
2016-12-28 18:32:44 +0300

    F*ng toolbar

--- app/src/main/java/com/khavronsky/blogreader/FirstFragment.java
+++ app/src/main/java/com/khavronsky/blogreader/FirstFragment.java
@@ -8,9 +8,11 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+
 public class FirstFragment extends Fragment {
     TextView textView;
     ImageButton imageButton;
+    private static final String TAG = "Fragment_1";
 
     public FirstFragment() {
     }
@@ -33,15 +35,18 @@ public class FirstFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_first, container, false);
         textView = (TextView) view.findViewById(R.id.tvFragment1);
         imageButton = (ImageButton) view.findViewById(R.id.first_button);
+
+
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                ((MainActivity) getActivity()).startSecondFragment();
+                MainActivity mainActivity=(MainActivity)getActivity();
+                mainActivity.setDrawerIndicatorEnabled(false);
+
                 ((MainActivity) getActivity()).startFragment(((MainActivity) getActivity()).secondFragment);
 
             }
         });
-
         return view;
     }
 }
--- app/src/main/java/com/khavronsky/blogreader/MainActivity.java
+++ app/src/main/java/com/khavronsky/blogreader/MainActivity.java
@@ -3,6 +3,7 @@ package com.khavronsky.blogreader;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,19 +26,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     ActionBarDrawerToggle toggle;
     DrawerLayout drawer;
     FragmentTransaction fragmentTransaction;
+    FragmentManager fragmentManager;
     Fragment firstFragment;
     Fragment secondFragment;
     Fragment thirdFragment;
     Fragment fourthFragment;
     NavigationView navigationView;
+    private static final String TAG = "MAIN ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
         setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
         toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -46,19 +53,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     private void init() {
+        Log.d(TAG, "init: start");
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         firstFragment = new FirstFragment();
         secondFragment = new SecondFragment();
         thirdFragment = new ThirdFragment();
         fourthFragment = new FourthFragment();
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Log.d(TAG, "onNavigationItemSelected: start");
         int id = item.getItemId();
         switch (id) {
             case (R.id.fragment1):
@@ -77,8 +84,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        Log.d(TAG, "onNavigationItemSelected: DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);");
         drawer.closeDrawer(GravityCompat.START);
+        Log.d(TAG, "onNavigationItemSelected: drawer.closeDrawer(GravityCompat.START);");
         return true;
+    }
+
+    public void setDrawerIndicatorEnabled(boolean set) {
+        this.toggle.setDrawerIndicatorEnabled(set);
     }
 
     public void onFragmentButtonSelected(View v) {
@@ -102,60 +115,99 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     public void startFragment(Fragment fragment) {
-        fragmentTransaction = getFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.container, fragment);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+        fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit();
+
+
+//        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("...", "OnClick work!");
+//                if (getFragmentManager().getBackStackEntryCount() > 0) {
+//                    Log.d(TAG, "ONCLICK onBackPressed: ");
+//                    onBackPressed();
+//                } else {
+////                    getSupportActionBar().setDisplayShowHomeEnabled(false);
+////                    Log.d(TAG, "setDrawerIndicatorEnabled: ");
+//                    toggle.setDrawerIndicatorEnabled(false);
+//                    toggle.setDrawerIndicatorEnabled(true);
+//                    drawer.addDrawerListener(toggle);
+//                    toggle.syncState();
+//                    navigationView.setNavigationItemSelectedListener(MainActivity.this);
+//
+////                    drawer.
+////                    toggle.syncState();
+////                    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+////                    drawer.openDrawer(GravityCompat.START, true);
+//
+//                }
+//
+//            }
+//        });
     }
 
     @Override
     public void onBackPressed() {
+        Log.d(TAG, "onBackPressed: start");
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        Log.d(TAG, "onBackPressed: DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);");
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
+            Log.d(TAG, "onBackPressed: drawer.closeDrawer(GravityCompat.START);");
+        } else if (getFragmentManager().getBackStackEntryCount() > 0) {
+//            Log.d(TAG, "onBackPressed: ");
             super.onBackPressed();
+        } else {
+
         }
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu: start");
 
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
     protected Dialog onCreateDialog(int id) {
-            AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
 
-            adb.setTitle("RED BO-O-OM BUTTON");
+        adb.setTitle("RED BO-O-OM BUTTON");
 
-            adb.setMessage("Oh, no!!!\n You pressed this fucking red button!!!");
+        adb.setMessage("Oh, no!!!\n You pressed this fucking red button!!!");
 
-            adb.setIcon(R.mipmap.scare);
+        adb.setIcon(R.mipmap.scare);
 
-            adb.setPositiveButton("Everybody must die", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    finish();
-                }
-            });
-            adb.setNegativeButton("I'm so sorry", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Toast toast = Toast.makeText(getApplicationContext(),
-                            "NO! Everybody must die", Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.CENTER, 0, 0);
-                    toast.show();
-                    finish();
-                }
-            });
-            return adb.create();
+        adb.setPositiveButton("Everybody must die", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        adb.setNegativeButton("I'm so sorry", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "NO! Everybody must die", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                finish();
+            }
+        });
+        return adb.create();
 
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(TAG, "onOptionsItemSelected: start");
 
         int id = item.getItemId();
 
